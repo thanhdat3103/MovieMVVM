a8c2fc455c9be33cb7ac494e41c44a93ad9a2d0e
thanhdat3103 <atnguyn79@gmail.com>
2023-05-18 00:57:51 +0700

    Generate Movie Details

---
+++ app/src/main/java/com/example/moviemvvm/data/vo/MovieDetail.kt
@@ -0,0 +1,25 @@
+package com.example.moviemvvm.data.vo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetail(
+    val budget: Int,
+    val homepage: String,
+    val id: Int,
+    @SerializedName("imdb_id")
+    val imdbId: String,
+    val overview: String,
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    val revenue: Int,
+    val runtime: Int,
+    val tagline: String,
+    val status: String,
+    @SerializedName("vote_average")
+    val rating: String,
+    val title: String
+)
